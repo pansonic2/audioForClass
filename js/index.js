839a59cecf05846cfbbd6bbe57b179523124fbcb
pansonic2 <30543241+pansonic2@users.noreply.github.com>
2019-02-06 19:03:59 +0500

    more audio

--- js/index.js
+++ js/index.js
@@ -1,4 +1,9 @@
-var x = document.getElementById("myAudio1"); 
+var x = document.getElementById("myAudio1");
+var y = document.getElementById("myAudio2");
+var x3 = document.getElementById("myAudio3");
+var x4 = document.getElementById("myAudio4");
+var x5 = document.getElementById("myAudio5");
+
 
 function playAudio() { 
   x.load();
@@ -7,4 +12,40 @@ function playAudio() {
 
 function pauseAudio() { 
   x.load(); 
+}
+
+function playAudio2() { 
+  y.load();
+  y.play(); 
+} 
+
+function pauseAudio2() { 
+  y.load(); 
+}
+
+function playAudio3() { 
+  x3.load();
+  x3.play(); 
+} 
+
+function pauseAudio3() { 
+  x3.load(); 
+}
+
+function playAudio4() { 
+  x4.load();
+  x4.play(); 
+} 
+
+function pauseAudio4() { 
+  x4.load(); 
+}
+
+function playAudio5() { 
+  x5.load();
+  x5.play(); 
+} 
+
+function pauseAudio5() { 
+  x5.load(); 
 }
